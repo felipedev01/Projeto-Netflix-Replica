07b0b1120b86e1d6670a77516f3e219fbd8199f0
Felipe <felipecss88@gmail.com>
2022-06-24 20:16:00 -0300

    Lista de objetos para exibição criada

--- src/Tmdb.jsx
+++ src/Tmdb.jsx
@@ -1,10 +1,52 @@
 const API_KEY = 'de1ec00c499ccad247802d2ca58aba55'
 const API_BASE= 'https://api.themoviedb.org/3'
 
-export function Tmdb(){
+export default{
 
-    return(
-
+    getHomeList: async () => {
+        return [
         
-    )
+        {
+            slug: 'originals',
+            title: 'Originais do Netflix',
+            items: []
+        },
+        {
+            slug: 'trending',
+            title: 'Recomendados para você',
+            items: []
+        },
+        {
+            slug: 'toprated',
+            title: 'Em Alta',
+            items: []
+        },
+        {
+            slug: 'action',
+            title: 'ação',
+            items: []
+        },
+        {
+            slug: 'comedy',
+            title: 'Comédia',
+            items: []
+        },
+        {
+            slug: 'horror',
+            title: 'Terror',
+            items: []
+        },
+        {
+            slug: 'romance',
+            title: 'Romance',
+            items: []
+        },
+        {
+            slug: 'documentary',
+            title: 'Documentários',
+            items: []
+        }
+
+        ]
+    }
 }
